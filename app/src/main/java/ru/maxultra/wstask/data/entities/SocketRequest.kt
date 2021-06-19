14df4f2025684029810bc2a640910c16f053e3cd
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-06-19 20:42:11 +0300

    Network-related stuff added

---
+++ app/src/main/java/ru/maxultra/wstask/data/entities/SocketRequest.kt
@@ -0,0 +1,3 @@
+package ru.maxultra.wstask.data.entities
+
+data class SocketRequest(val method: String, val params: List<String>, val id: Int)
---
+++ app/src/main/java/ru/maxultra/wstask/data/network/ApiConstants.kt
@@ -0,0 +1,9 @@
+package ru.maxultra.wstask.data.network
+
+const val WSS_ENDPOINT = "wss://stream.binance.com:9443/ws"
+const val API_ENDPOINT = "https://api.binance.com"
+
+const val METHOD_SUBSCRIBE = "SUBSCRIBE"
+const val METHOD_UNSUBSCRIBE = "UNSUBSCRIBE"
+
+const val PARAM_DEPTH = "@depth"
